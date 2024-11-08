86c1c89a3c5c18e020348bc5331ef09720f240b3
Pulkit0707 <97471873+Pulkit0707@users.noreply.github.com>
2024-11-08 15:33:01 +0530

    Add files via upload

---
+++ web3/hardhat.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://sepolia.rpc.thirdweb.com',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
